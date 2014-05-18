5ae828573f851c60a0e56190ab62f450d7327f4b
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-05-19 06:04:41 +0900

    Update website.

---
+++ releases/nopicads.meta.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name           NoPicAds
+// @namespace      FoolproofProject
+// @description    No Picture Advertisements
+// @copyright      2012+, legnaleurc (https://github.com/legnaleurc/nopicads)
+// @version        4.32.0
+// @license        BSD
+// @updateURL      https://legnaleurc.github.io/nopicads/releases/nopicads.meta.js
+// @downloadURL    https://legnaleurc.github.io/nopicads/releases/nopicads.user.js
+// @grant          unsafeWindow
+// @grant          GM_xmlhttpRequest
+// @grant          GM_addStyle
+// @grant          GM_getResourceText
+// @grant          GM_getResourceURL
+// @grant          GM_getValue
+// @grant          GM_openInTab
+// @grant          GM_registerMenuCommand
+// @grant          GM_setValue
+// @run-at         document-start
+// @resource       alignCenter https://raw.github.com/legnaleurc/nopicads/v4.32.0/css/align_center.css
+// @resource       scaleImage https://raw.github.com/legnaleurc/nopicads/v4.32.0/css/scale_image.css
+// @resource       bgImage https://raw.github.com/legnaleurc/nopicads/v4.32.0/img/imagedoc-darknoise.png
+// @include        http://*
+// @include        https://*
+// ==/UserScript==
